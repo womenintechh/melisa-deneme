846d07765aed4317d13adaef0ee33252b67372db
melisaadakli <104603594+melisaadakli@users.noreply.github.com>
2024-03-20 18:22:46 +0300

    Add files via upload

---
+++ web-scrapping-app.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def web_scraping(url):
+    # Web sitesinden sayfayı çeker
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        # HTML içeriğini BeautifulSoup ile analiz eder
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Sayfa içeriğinden istenilen veriyi çeker
+        # Tüm metin içeriğini alır
+        haber_basliklari = soup.find_all('h2', class_='news-title')  # Sayfanın HTML yapısına göre uygun etiket ve sınıfı belirler
+        
+        # Sonucu ekrana basar
+        for baslik in haber_basliklari:
+            print(baslik.get_text().strip())
+    else:
+        print(f"Hata: {response.status_code} - Sayfa çekilemedi.")
+
+if __name__ == "__main__":
+    websitesi_url = "url buraya yapıştır"
+    web_scraping(websitesi_url)
